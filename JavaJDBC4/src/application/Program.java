ae986463cb619c9994f43c4104f97b355b253278
Pablo Tokuo <pbo_1996@hotmail.com>
2022-11-29 12:01:08 -0300

    Atualizar Dados

---
+++ JavaJDBC4/src/application/Program.java
@@ -0,0 +1,31 @@
+package application;
+
+import db.DB;
+
+import java.sql.*;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Connection conn = null;
+//        Forma de fazer insercao no banco de dados mais segura onde vc prepara os parametros para serem inseridos.
+        PreparedStatement st = null;
+        try {
+            conn = DB.getConnection();
+            st = conn.prepareStatement("UPDATE seller " + "SET BaseSalary = BaseSalary + ? " + "WHERE " + "(DepartmentId = ?)");
+
+            st.setDouble(1, 200.0);
+            st.setInt(2, 2);
+
+            int rowsAffected = st.executeUpdate();
+            System.out.println("Done! Rows Affected: " + rowsAffected);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+
+    }
+}
